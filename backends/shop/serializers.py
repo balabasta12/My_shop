a0145ddad269573ab3df01938f234813aea0ddda
balabasta12 <balabasta12@gmail.com>
2022-12-13 15:23:46 +0300

    Добавил CategorySerializer, ProductListSerializer

--- backends/shop/serializers.py
+++ backends/shop/serializers.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
-from shop.models import Shop
+from shop.models import Shop, Product, Category
 
 
 class ShopSerializer(ModelSerializer):
     class Meta:
         model = Shop
-        fields = ('id', 'name')
+        fields = ('id', 'name', 'url', 'state',)
+
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name',)
+
+
+class ProductListSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'category',)
