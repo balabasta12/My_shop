8e804e1f8cd33fc142be7c31756447ef19eaf7c0
balabasta12 <balabasta12@gmail.com>
2022-12-13 15:22:16 +0300

    Добавил как в примере

--- backends/shop/admin.py
+++ backends/shop/admin.py
@@ -1,3 +1,73 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from shop.models import Shop, Category, Product, ProductInfo, Parameter, ProductParameter, Order, OrderItem, \
+    Contact, ConfirmEmailToken, Us
+
+
+@admin.register(Us)
+class CustomUserAdmin(UserAdmin):
+    """
+    Панель управления пользователями
+    """
+    model = Us
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'type')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'company', 'position')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductInfo)
+class ProductInfoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductParameter)
+class ProductParameterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+
+# @admin.register(ConfirmEmailToken)
+# class ConfirmEmailTokenAdmin(admin.ModelAdmin):
+#     = = ('user', 'key', 'created_at',)
